272f1e9f06ea09fb024078c4e5c3cb8ed294142a
Walderlan Sena <senawalderlan@gmail.com>
2017-05-16 00:59:38 -0300

    Adicionados arquivos da 1° aula do curso

---
+++ aula00/olamundo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Mini Curso - Introdução ao Shell Script
+# Site: https://www.mentesvirtuaissena.com/
+# contato@mentesvirtuaissena.com
+# Aula00 - Apresentação do Curso...
+#
+clear # Limpando a tela do terminal...
+# Imprimindo mensagem na tela
+echo "Bem Vindo ao Curso de Intrudução ao Shell"
+echo "Ola Mundo !!!"
